c1c12f9ecd7214c84b0f5af88c4891671f777e1d
wangfy0508 <fayin1004@gmail.com>
2020-11-15 12:07:26 +0800

    add HadoopDemo

---
+++ src/main/java/wordCount/WordCountMapper.java
@@ -0,0 +1,31 @@
+package wordCount;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+/**
+ * @Description
+ * @Package wordCount
+ * @Author wfy
+ * @Version V1.0.0
+ * @Date 2020/11/13 17:28
+ */
+
+public class WordCountMapper extends Mapper<Object, Text, Text, IntWritable> {
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    @Override
+    protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        StringTokenizer itr = new StringTokenizer(value.toString());
+        while (itr.hasMoreTokens()) {
+            word.set(itr.nextToken());
+            context.write(word, one);
+        }
+    }
+
+}
