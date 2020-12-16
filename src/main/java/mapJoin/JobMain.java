8b5b5949290c18731294aaffd61b5c3f813b596b
wangfy0508 <fayin1004@gmail.com>
2020-12-16 10:04:45 +0800

    add mapJoin

--- src/main/java/mapJoin/JobMain.java
+++ src/main/java/mapJoin/JobMain.java
@@ -3,9 +3,17 @@ package mapJoin;
 import org.apache.hadoop.conf.Configurable;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.filecache.DistributedCache;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
+
+import java.net.URI;
 
 /**
  * @Description
@@ -17,13 +25,39 @@ import org.apache.hadoop.util.ToolRunner;
 
 public class JobMain extends Configured implements Tool {
     String hdfsUri = "hdfs://192.168.168.10:9000";
+
     @Override
     public int run(String[] strings) throws Exception {
         // 1. 获取Job实例
         Job job = Job.getInstance(super.getConf(), "map_join");
         job.setJar("HadoopDemo-1.0-SNAPSHOT.jar");
 
-        return 0;
+        // 2. 将小表（商品表）读入分布式内存
+        //DistributedCache.addCacheFile(new URI(hdfsUri+"/cache_files/product.txt"), super.getConf());
+        job.addCacheFile(new URI(hdfsUri + "/cache_files/product.txt"));
+
+        // 3. 设置读取文件使用的类和输入路径
+        job.setInputFormatClass(TextInputFormat.class);
+        TextInputFormat.addInputPath(job, new Path(hdfsUri + "/map_join_input"));
+
+        // 4. 设置Mapper类和输出的键值对类型
+        job.setMapperClass(MapJoinMapper.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
+
+        // 5. 设置输出的文件格式和输出路径
+        job.setOutputFormatClass(TextOutputFormat.class);
+        TextOutputFormat.setOutputPath(job, new Path(hdfsUri + "/map_join_output"));
+        FileSystem fileSystem = FileSystem.get(URI.create(hdfsUri), new Configuration());
+        Path outputPath = new Path(hdfsUri + "/map_join_output");
+        boolean exists = fileSystem.exists(outputPath);
+        if (exists) {
+            fileSystem.delete(outputPath, true);
+        }
+
+        // 6. 等待程序执行完成
+        boolean completion = job.waitForCompletion(true);
+        return completion ? 0 : 1;
     }
 
     public static void main(String[] args) throws Exception {
